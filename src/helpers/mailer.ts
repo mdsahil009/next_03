dd67b2c4586127955bdafac3f835260a73fdf12b
mdsahil009 <mohdsahil00736@gmail.com>
2025-08-21 12:34:36 +0530

    add another next project

---
+++ src/helpers/mailer.ts
@@ -0,0 +1,55 @@
+import nodemailer from 'nodemailer'
+import User from '@/models/userModel'
+import bcryptjs from 'bcryptjs';
+
+export const sendEmail = async ({email, emailType, userId}:any) => {
+    
+    try {
+      const hashedToken = await bcryptjs.hash(userId.toString() , 10)
+
+      if (emailType === "VERIFY"){
+         const updatedUser = await User.findbyIdAndUpdate(userId, {
+            $set : {
+              verifyToken : hashedToken, 
+              verifyTokenExpairy: new Date(Date.now() + 3600000)
+             }
+          });
+
+       } else if (emailType === "Reset"){
+          await User.'findbyIdAndUpdate'(userId, {
+            $set : {
+              forgotPasswordToken : hashedToken,
+              forgotPasswordTokenExpairy:  new Date(Date.now() + 3600000)
+            }
+          });
+       }
+
+        // Looking to send emails in production? Check out our Email API/SMTP product!
+          var transport = nodemailer.createTransport({
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+              user: "ab98a6fd66c0fa",   //  not here 
+              pass: "e0df967a205cd5", 
+            }
+          });
+
+          const mailOptions = {
+            from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
+            to: email,
+            subject: emailType === 'verify' ? "Verify your email" : "Reset your password",
+            html: `<p> Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here </a> to ${emailType === "VERIFY" ? "Verify your email" : "reset your password"}
+            or copy or paste the link in the browser.
+            <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+            </p>`, 
+          }
+
+          const mailResponse = await transport.sendMail(mailOptions)
+          return mailResponse
+
+
+    } catch (error : any) {
+        throw new Error(error.message)
+    }
+}
+
